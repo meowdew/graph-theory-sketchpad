65cbfaccc9f5e1db7c92d3c51293a081e7d6d654
meowdew <redmnq77249@gmail.com>
2023-11-24 21:52:48 -0800

    implemented updating nodes label

--- src/components/network/index.jsx
+++ src/components/network/index.jsx
@@ -1,11 +1,15 @@
 import React, { useEffect, useRef, useState } from "react";
 import { DataSet, Network } from "vis-network/standalone";
 import PubSub from "pubsub-js";
+import { Input, Modal } from "antd";
 
 const Graph = (props) => {
-  const [nodes, setNodes] = useState(new DataSet([]));
-  const [edges, setEdges] = useState(new DataSet([]));
-  const networkRef = useRef(null);
+  const nodes = useRef(new DataSet([])).current;
+  const edges = useRef(new DataSet([])).current;
+  const network = useRef(null);
+  const [openLabelEdit, setLabelEditOpen] = useState(false);
+  const [newLabel, setNewLabel] = useState(null);
+  const [nodeId, setNodeId] = useState(null);
 
   let options = {
     interaction: {
@@ -18,16 +22,35 @@ const Graph = (props) => {
     manipulation: {
       enabled: true,
       addNode: (node, callback) => {
-        node = {
-          ...node,
-          id: nodes.length + 1,
-          label: `Node ${nodes.length + 1}`,
-        };
+        nodes.length === 0
+          ? (node = { id: 0, label: "0" })
+          : (node = {
+              id: nodes.getIds()[nodes.length - 1] + 1,
+              label: ` ${nodes.getIds()[nodes.length - 1] + 1} `,
+            });
         callback(node); //add
         PubSub.publish("nodes-length", { len: nodes.length });
       },
       addEdge: (edge, callback) => {
+        edge = {
+          ...edge,
+          from: parseInt(edge.from),
+          to: parseInt(edge.to),
+        };
         callback(edge); //add
+      },
+      deleteNode: (data, callback) => {
+        if (data.nodes.length > 0) {
+          data.nodes.forEach((nodeId) => {
+            nodes.remove({ id: nodeId });
+            data.edges.forEach((edge) => {
+              if (edge.from === nodeId || edge.to === nodeId) {
+                edges.remove({ id: edge.id });
+              }
+            });
+          });
+        }
+        PubSub.publish("nodes-length", { len: nodes.length });
       },
     },
   };
@@ -37,10 +60,13 @@ const Graph = (props) => {
       "activate-add-node",
       (msg, data) => {
         if (data) {
-          const newNode = {
-            id: nodes.length + 1,
-            label: `Node ${nodes.length + 1}`,
-          };
+          let newNode = {};
+          nodes.length === 0
+            ? (newNode = { id: 0, label: "0" })
+            : (newNode = {
+                id: nodes.getIds()[nodes.length - 1] + 1,
+                label: ` ${nodes.getIds()[nodes.length - 1] + 1} `,
+              });
           nodes.add(newNode);
           PubSub.publish("nodes-length", { len: nodes.length });
         }
@@ -50,7 +76,9 @@ const Graph = (props) => {
     const addEdgeSubscription = PubSub.subscribe(
       "edge-add-ready",
       (msg, data) => {
-        edges.add(data);
+        if (data) {
+          edges.add(data);
+        }
       },
     );
 
@@ -62,21 +90,59 @@ const Graph = (props) => {
 
   useEffect(() => {
     const container = document.getElementById("network");
-    console.log(`nodes`, nodes);
-    console.log(`edges`, edges);
-    networkRef.current = new Network(
-      container,
-      { nodes, edges },
-      { ...options },
-    );
-    networkRef.current.on("click", (params) => {});
+    network.currnet = new Network(container, { nodes, edges }, { ...options });
+
+    network.currnet.on("doubleClick", (params) => {
+      //double click a node
+      if (params.nodes.length > 0) {
+        const nodeId = params.nodes[0];
+        const currentNode = nodes.get(nodeId);
+        showEditDialog(nodeId);
+      }
+    });
   }, []);
+
+  const showEditDialog = (nodeId) => {
+    setLabelEditOpen(true);
+    setNodeId(nodeId);
+  };
+
+  const handleLabelChange = (e) => {
+    setNewLabel(e.target.value);
+  };
+
+  const handleEditDialogCancel = () => {
+    setNewLabel(null);
+    setNodeId(null);
+    setLabelEditOpen(false);
+  };
+
+  const handleEditDialogOk = () => {
+    setLabelEditOpen(false);
+    nodes.update({ id: nodeId, label: newLabel });
+    setNodeId(null);
+    setNewLabel(null);
+  };
 
   return (
     <div
       id={"network"}
       className={"border-blue-100 border-2 h-5/6 bg-yellow-50 rounded-lg mx-4"}
-    ></div>
+    >
+      <Modal
+        title={"Edit Node Label"}
+        className={"node-label-edit"}
+        open={openLabelEdit}
+        onCancel={handleEditDialogCancel}
+        onOk={handleEditDialogOk}
+      >
+        <Input
+          onChange={handleLabelChange}
+          placeholder={"Please enter your new label"}
+          value={newLabel}
+        />
+      </Modal>
+    </div>
   );
 };
 
